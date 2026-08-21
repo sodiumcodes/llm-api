6e07b8307458f428f0d5a50e78d11b8214fdb8fd
nainascode <nainadugar.official@gmail.com>
2026-08-21 15:37:19 +0530

    feat: update chat flow with user prompts and assistant responses

--- main.py
+++ main.py
@@ -12,12 +12,35 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key= api_key
 )
+messages = []
 
+messages.append({
+    "role": "user",
+    "content": "My name is Naina and I am participating in the Razorpay Buildathon."
+})
 response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=[{
-        "role": "user",
-        "content": "What is an API?"   
-    }]
+    model="openrouter/free", 
+    #openrouter/free acts as a router. It can select an available free model to handle your request.
+    max_tokens=500,
+    temperature= 0.1,
+    messages= messages
 )
-print(response.choices[0].message.content)
+assistant_response = response1.choices[0].message.content
+
+messages.append({
+    "role": "assistant",
+    "content": assistant_response
+})
+
+messages.append({
+    "role": "user",
+    "content": "Do you have any suggesstions?"
+})
+response1 = client.chat.completions.create(
+    model="openrouter/free", 
+    #openrouter/free acts as a router. It can select an available free model to handle your request.
+    max_tokens=500,
+    temperature= 0.1,
+    messages= messages
+)
+print(response1.choices[0].message.content) #next chat response, not the messages array
