36574e31238c0fa4e9584de784d58d634c476198
nainascode <nainadugar.official@gmail.com>
2026-08-21 15:12:41 +0530

    feat: implement OpenAI client and add example API call

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+# *debug
+# print (api_key);
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key= api_key
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[{
+        "role": "user",
+        "content": "What is an API?"   
+    }]
+)
+print(response.choices[0].message.content)
